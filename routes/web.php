9d1ae47330e08ce5aea6ae56be1f5e2411f00bf4
Richard Bernisca <taropogi_123@yahoo.com>
2021-05-15 14:24:44 +0800

    use single laravel route

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-})->middleware('guest');
+})->where('any', '.*')->middleware('guest');
 
 
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
