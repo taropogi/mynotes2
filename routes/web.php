91aa2664de531b03db431b60f44b63c4ade2e4ed
Richard Bernisca <taropogi_123@yahoo.com>
2021-05-15 12:13:09 +0800

    Goto 883ed7e initial

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->middleware('guest');
-
-
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
